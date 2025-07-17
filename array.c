b39fd3a3d85f358ec4dcd87d7212270c7fc0309c
GIT INDIA <gitechno.in@gmail.com>
2025-07-17 11:08:36 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+
+// The main difference between int main() and void main() lies in their return type. int main() is the standard and preferred way to define the main function in C and C++, indicating that it returns an integer value, typically used to signal the program's exit status to the operating system. void main() is a non-standard declaration that does not return any value, and its use is generally discouraged as it can lead to undefined behavior in some cases.
+
+// int main(){
+// printf("this is testing program using void main()");
+
+// return 0;
+
+// }
+
+
+/* void main(){
+printf("this is testing program using void main()");
+
+return 1;
+printf("this is final testing!!");
+} */
+
+#include<stdio.h>
+
+// An array is a variable that can store multiple values. For example, if you want to store 100 integers, you can create an array for it.
+int main(){
+
+// int arr[7]; // empty array \
+
+// arr[0] = 45;
+// arr[1] = 61;
+// arr[2] = 29;
+// arr[3] = 98;
+// arr[4] = 76;
+// arr[5] = 45;
+// arr[6] = 92;
+
+// // printf("%d",arr[0]);
+
+// int size = sizeof(arr) / sizeof(arr[0]);
+
+// int i = 0;
+// for(i;i<size;i++){
+//     printf("%d ",arr[i]);
+// }
+
+// char a[] = {"Tech"};
+
+// int size = sizeof(a) / sizeof(a[0]);
+
+// int i =0;
+// for(i;i<size;i++){
+//     printf("%c ",a[i]);
+// }
+
+// char *a[] = {"Tech","Programming","language","C","C++"};
+
+// int size = sizeof(a) / sizeof(a[0]);
+
+// int i =0;
+// for(i;i<size;i++){
+//     printf("%s ",a[i]);
+// }
+
+
+
+}
+
