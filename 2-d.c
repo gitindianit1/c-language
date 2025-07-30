010526fc9000a86cc1eff6652a6ddf06c8e3bc39
GIT INDIA <gitechno.in@gmail.com>
2025-07-30 12:09:24 +0530

    Add files via upload

---
+++ 2-d.c
@@ -0,0 +1,73 @@
+/*
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("1. BASIC ARRAY CONCEPTS\n");
+    printf("----------------------\n");
+
+    // Declaration and initialization
+    int numbers[5] = {10, 20, 30, 40, 50};
+    float decimals[3] = {1.1f, 2.2f, 3.3f};
+    char vowels[5] = {'a', 'e', 'i', 'o', 'u'};
+    char *a[] = {"Tech","Programming","language","C","C++"};
+
+    // Accessing elements
+    printf("Integer array: ");
+    for(int i = 0; i < 5; i++) {
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
+
+    // Partial initialization
+    int partial[10] = {1, 2, 3};  // Rest initialized to 0
+    printf("Partial initialization: ");
+    for(int i = 0; i < 10; i++) {
+        printf("%d ", partial[i]);
+    }
+    printf("\n");
+
+    // Size determination
+    printf("Size of numbers array: %zu elements\n", sizeof(numbers)/sizeof(numbers[0]));
+
+    // Memory layout
+    printf("Memory addresses:\n");
+    for(int i = 0; i < 5; i++) {
+        printf("numbers[%d] = %d at address %p\n", i, numbers[i], (void*)&numbers[i]);
+    }
+    printf("\n");
+
+    return 0;
+} */
+
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("1. Multi-Dimensional ARRAY CONCEPTS\n");
+    printf("----------------------\n");
+
+    float y[2][4] = {
+    { 1,2,3,4},
+    { 5,6,7,8}
+    };
+
+    printf("\n%1.f",y[0][1]);
+    printf("\n%1.f",y[1][2]);
+    printf("\n%1.f",y[1][1]);
+
+// Change value
+    y[1][0] = 56;
+    printf("\n%1.f",y[1][0]);
+
+  // 7 replace by 78
+y[1][2] = 78;
+printf("\n%1.f",y[1][2]);
+
+    return 0;
+}
+
+
